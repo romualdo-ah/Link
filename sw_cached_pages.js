04ba37a18bccb59985fd89fdd9fb1096497e835b
psianacarolina <75583158+psianacarolina@users.noreply.github.com>
2020-12-18 13:55:03 -0300

    Update sw_cached_pages.js

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,19 +1,7 @@
 const cacheName = "v1";
-const cacheAssets = ["index.html", "transparent_logo.png","sw_cached_pages.js"];
 
 self.addEventListener("install", (e) => {
   console.log("ServiceWorker installed");
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("ServiceWorker caching files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
 
 self.addEventListener("activate", (e) => {
   console.log("ServiceWorker activated");
@@ -35,8 +23,16 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   console.log("ServiceWorker fetching");
   e.respondWith(
-    fetch(e.request).catch(() => {
-      caches.match(e.request);
-    })
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => {
+        caches.match(e.request).then((res) => res);
+      })
   );
 });
